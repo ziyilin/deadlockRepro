61ec6b9307af4f5a2235f40e2382c901972d8c8d
kejunxiao <xkjmen@163.com>
2015-02-05 10:51:17 +0800

    clean the files in the old packages

--- src/main/java/cfg/LockNode.java
+++
@@ -1,9 +0,0 @@
-package cfg;
-
-import soot.Unit;
-
-public class LockNode extends StmtNode{
-	public LockNode(Unit u){
-		super(u,Type.Lock);
-	}
-}
--- src/main/java/cfg/UnlockNode.java
+++
@@ -1,9 +0,0 @@
-package cfg;
-
-import soot.Unit;
-
-public class UnlockNode extends StmtNode{
-	public UnlockNode(Unit u){
-		super(u, Type.Unlock);
-	}
-}
