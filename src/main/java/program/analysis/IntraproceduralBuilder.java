c03fd99ca8c1ede3a8e2356a57e693a72f75d67c
kejunxiao <xkjmen@163.com>
2015-02-06 22:17:23 +0800

    remove redundant check

--- src/main/java/program/analysis/IntraproceduralBuilder.java
+++ src/main/java/program/analysis/IntraproceduralBuilder.java
@@ -22,9 +22,8 @@ import soot.util.Chain;
 import thread.builder.ThreadBuilder;
 import thread.info.SiteInfo;
 import thread.info.ThreadInfo;
-
 public class IntraproceduralBuilder {
-	public static Map<SootMethod,UnitGraph> methodToCFG=new HashMap<SootMethod,UnitGraph>();
+	public static Map<SootMethod,UnitGraph> methodToUnitGraph=new HashMap<SootMethod,UnitGraph>();
 	public static Collection<ThreadInfo> Main(String dumpFile,String mainClass)
 			throws FileNotFoundException{
 		
@@ -35,145 +34,63 @@ public class IntraproceduralBuilder {
 		Collection<ThreadInfo> threadInfoCollection=b.build(mainClass,threadDump.values());
 		return threadInfoCollection;
 	}
-	public Collection<ThreadInfo> build(String benchmark,final Collection<ThreadInfo> threadInfoCollection){
-		
-		List<String> argsList = new ArrayList<String>();
-/*		Boolean interProcedural=false;
-		if(interProcedural){
-		
-			argsList.addAll(Arrays.asList(new String[]{
-					"-w","--app",benchmark,
-					"-allow-phantom-refs",
-					"-keep-line-number",
-					"-print-tags-in-output",
-	//				"-via-shimple",
-					"-p","wjtp","enabled:true",
-					"-p", "wjtp.bcfg","on",
-					"-src-prec","java",
-					"-f","n",
-					"-main-class", benchmark,benchmark
-			}));
-			PackManager.v().getPack("wjtp")
-			.add(new Transform("wjtp.bcfg", new SceneTransformer() {
-				@Override
-				protected void internalTransform(String phaseName,
-						Map options) {
-	
-							Chain<SootClass> cl = Scene.v().getApplicationClasses();
-							for (SootClass sClass : cl) {
-								List<SootMethod> ml = sClass.getMethods();
-//								MethodLoop:
-								for (SootMethod sMethod : ml) {
-									// Initialize some information: method, class
-									
-									String methodName = sMethod.getName();
-									String classPath = sClass.getName();
-									String clsMetInfo = classPath + "."
-											+ methodName;
 
-									
-									
-									
-//									if (methToLineNumber.containsKey(clsMetInfo) == false)
-//										continue MethodLoop;
-									
-//									Integer lineNumber=methToLineNumber.get(clsMetInfo);
-									
-									if (sMethod.hasActiveBody() == false)
-										sMethod.retrieveActiveBody();
-									Body body = sMethod.getActiveBody();
-									
+	public Collection<ThreadInfo> build(String benchmark,final Collection<ThreadInfo> threadInfoCollection){
+		List<String> argsList = new ArrayList<String>();
+		G.reset();
+		argsList.addAll(Arrays.asList(new String[]{
+				"--app",benchmark,
+				"-allow-phantom-refs" ,
+				"-keep-line-number",
+//					"-print-tags-in-output",
+				"-via-shimple",
+				"-p","jtp","enabled:true",
+				"-p", "jtp.bcfg","on",
+				"-src-prec","java",
+				"-f","n",
+				"-main-class", benchmark,benchmark
+		}));
+		PackManager.v().getPack("jtp")
+				.add(new Transform("jtp.bcfg", new BodyTransformer() {
+					@Override
+					protected void internalTransform(Body body,
+							String phaseName, Map options) {
+						SootMethod sMethod = body.getMethod();
+						SootClass sClass = sMethod.getDeclaringClass();
+						String methodName = sMethod.getName();
+						String classPath = sClass.getName();
+						String clsMetInfo = classPath + "." + methodName;
+						
+						UnitGraph cfg = new SimpleExceptionalGraph(body);
+						
+						methodToUnitGraph.put(sMethod, cfg);
+						
+						for(ThreadInfo ti:threadInfoCollection){
+							List<SiteInfo> sl=ti.getSite();
+							
+							Site:
+							for(SiteInfo s:sl){
+								String methodCompleteName=s.getPackageName()+"."+s.getClassName()+"."+s.getMethodName();
+								
+								if(clsMetInfo.equals(methodCompleteName)){
+									Integer lineNumber=s.getLineNumber();
 									Chain<Unit> uChain=body.getUnits();
-									Boolean methodFound=false;
+									
+									
 									for(Unit u:uChain){
 										LineNumberTag lnt = (LineNumberTag)u.getTag("LineNumberTag");
-										Integer ln = lnt.getLineNumber();
-//										if(ln.equals(lineNumber)){
-											
-											methodFound=true;
-											
-											// Map invocation:Unit to callee:SootMethod
-											
-//										}
-									}
-									if(methodFound){
-										UnitGraph uGraph = new ExceptionalUnitGraph(body);
-//										methToUnitGraph.put(clsMetInfo, uGraph);
-									}
-								}
-							}
-						}
-			}));
-		}
-		else
-*/		{
-			G.reset();
-			argsList.addAll(Arrays.asList(new String[]{
-					"--app",benchmark,
-					"-allow-phantom-refs" ,
-					"-keep-line-number",
-//					"-print-tags-in-output",
-					"-via-shimple",
-//					"-p","wstp","enabled:true",
-					
-					"-p","jtp","enabled:true",
-					"-p", "jtp.bcfg","on",
-					"-src-prec","java",
-					"-f","n",
-					"-main-class", benchmark,benchmark
-			}));
-			PackManager.v().getPack("jtp")
-					.add(new Transform("jtp.bcfg", new BodyTransformer() {
-						@Override
-						protected void internalTransform(Body body,
-								String phaseName, Map options) {
-							SootMethod sMethod = body.getMethod();
-							SootClass sClass = sMethod.getDeclaringClass();
-							String methodName = sMethod.getName();
-							String classPath = sClass.getName();
-							String clsMetInfo = classPath + "." + methodName;
-							if (sMethod.hasActiveBody() == false)
-								sMethod.retrieveActiveBody();
-							UnitGraph cfg = new SimpleExceptionalGraph(body);
-							methodToCFG.put(sMethod, cfg);
-							
-							for(ThreadInfo ti:threadInfoCollection){
-								List<SiteInfo> sl=ti.getSite();
-								
-								Site:
-								for(SiteInfo s:sl){
-									String methodCompleteName=s.getPackageName()+"."+s.getClassName()+"."+s.getMethodName();
-									
-									if(clsMetInfo.equals(methodCompleteName)){
-										Integer lineNumber=s.getLineNumber();
-										Chain<Unit> uChain=body.getUnits();
-										
-										
-										for(Unit u:uChain){
-											LineNumberTag lnt = (LineNumberTag)u.getTag("LineNumberTag");
-											if(lineNumber.equals(lnt.getLineNumber())){
-											
-												// Set SootMethod and UnitGraph for the site
-												if(s.getUnitGraph()==null){
-//													UnitGraph cfg = new ExceptionalUnitGraph(body);
-													s.setUnitGraph(cfg);
-													s.setMethod(sMethod);
-												}
-												else if(!s.getMethod().equals(sMethod)){
-													System.err.println("Exception found: Duplicate settings of sites are different");
-												}
-												break Site;
-											}
+										if(lineNumber.equals(lnt.getLineNumber())){
+											// Set SootMethod and UnitGraph for the site
+											s.setUnitGraph(cfg);
+											s.setMethod(sMethod);
+											break Site;
 										}
 									}
 								}
 							}
-							
-						
-							
-						}
-			}));
-		}
+						}						
+					}
+		}));		
 		soot.Main.main(argsList.toArray(new String[0]));
 		return threadInfoCollection;
 	}
