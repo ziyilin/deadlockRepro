827a25e6adcf3f9fc53ff1b866c72eb64dbdf58e
kejunxiao <xkjmen@163.com>
2015-02-06 22:21:27 +0800

    test the building of cfg and print the cfg

--- src/test/java/cfg/builder/TestCFGBuilder.java
+++ src/test/java/cfg/builder/TestCFGBuilder.java
@@ -1,8 +1,16 @@
 package cfg.builder;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.ListenableDirectedGraph;
+
+import thread.info.ThreadInfo;
 
 import cfg.info.CFGNode;
 
@@ -10,6 +18,30 @@ public class TestCFGBuilder {
 	public static void main(String[] args) throws InterruptedException{
 		CFGBuilder cfgb=new CFGBuilder();
 		Graph<CFGNode, DefaultEdge> g=cfgb.build(new ListenableDirectedGraph<CFGNode, DefaultEdge>( DefaultEdge.class ));
-		System.out.println(g.toString());
+		for (ThreadInfo ti : cfgb.threadInfoCollection) {
+			System.out.println("-----"+ti.getThreadName()+"-----");
+			Stack<CFGNode> stack = new Stack<CFGNode>();
+			Set<CFGNode> visited = new HashSet<CFGNode>();
+			CFGNode curr = ti.getCfg().getHead().get(0);
+			visited.add(curr);
+			stack.push(curr);
+			while (stack.size() > 0) {
+				List<CFGNode> temp = new LinkedList<CFGNode>();
+				temp.addAll(stack);
+				while (stack.size() != 0) {
+					CFGNode p = stack.pop();
+					System.out.print(p+"\t\t");
+				}
+				System.out.println();
+				for (CFGNode t : temp) {
+					for (Object o : g.edgesOf(t)) {
+						DefaultEdge e = (DefaultEdge) o;
+						CFGNode succ = (CFGNode) g.getEdgeTarget(e);
+						if (visited.add(succ))
+							stack.push(succ);
+					}
+				}
+			}
+		}
 	}
 }
